b2dbdbe3de310b072d4bf8203e4b2cae0f04252b
emcahell <emmanuelltowers@gmail.com>
2025-05-21 10:56:32 -0400

    notificaciones en mobile

--- scripts/header.js
+++ scripts/header.js
@@ -1,19 +1,28 @@
 const btn = document.getElementById('btn-notification');
 const modalNotification = document.getElementById('notification-modal');
-const icon = document.getElementById('icon-notification');
+const iconNotification = document.getElementById('icon-notification');
 const openEdit = document.getElementById('open-modal-edit');
 const closedEdit = document.getElementById('close-modal-edit');
+const closedNotification = document.getElementById('icon-notification-back');
 const modalEdit = document.getElementById('edit-modal');
 
 // Modal notificación
 const togglePanelNotification = btn.addEventListener("click", () =>{
   if (modalNotification.classList.contains('none')){
       modalNotification.classList.remove('none');
-      icon.classList.add('fill-notification');
+      iconNotification.classList.add('fill-notification');
       modalNotification.classList.add("block");
   } else {
-      icon.classList.remove('fill-notification');
+      iconNotification.classList.remove('fill-notification');
       modalNotification.classList.remove("block");
+      modalNotification.classList.add("none");
+  }
+})
+
+const closedNotificationPanel = closedNotification.addEventListener("click", () => {
+  if (modalNotification.classList.contains('block')){
+      modalNotification.classList.remove("block");
+      iconNotification.classList.remove('fill-notification');
       modalNotification.classList.add("none");
   }
 })
