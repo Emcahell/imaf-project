e6434cce9a48b1b7a7e91f5d47b10ed8b57f0339
emcahell <emmanuelltowers@gmail.com>
2025-05-21 13:10:53 -0400

    header responsive

--- scripts/header.js
+++ scripts/header.js
@@ -1,6 +1,7 @@
 const btn = document.getElementById('btn-notification');
 const modalNotification = document.getElementById('notification-modal');
 const iconNotification = document.getElementById('icon-notification');
+const iconUser = document.getElementById('icon-user');
 const openEdit = document.getElementById('open-modal-edit');
 const closedEdit = document.getElementById('close-modal-edit');
 const closedNotification = document.getElementById('icon-notification-back');
@@ -32,12 +33,14 @@ const openPanelEdit = openEdit.addEventListener("click", () =>{
   if (modalEdit.classList.contains('none')){
       modalEdit.classList.remove('none');
       modalEdit.classList.add("block");
+      iconUser.classList.add('icon-modal-open');
   }
 })
 
 const closedPanelEdit = closedEdit.addEventListener("click", () =>{
   if (modalEdit.classList.contains('block')){
       modalEdit.classList.remove('block');
+      iconUser.classList.remove('icon-modal-open');
       modalEdit.classList.add("none");
   }
 })
