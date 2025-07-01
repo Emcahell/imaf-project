b1807557e0017763e7aaa82b0e4497349fc84f96
emcahell <emmanuelltowers@gmail.com>
2025-06-30 20:24:54 -0400

    feat: agrege una transicion suave al cambiar de paginas (pag auth)

--- scripts/confirm-recover.js
+++ scripts/confirm-recover.js
@@ -37,3 +37,24 @@ form.addEventListener('submit', async (e) => {
     console.error(error);
   }
 });
+
+// Fade in del body
+document.addEventListener('DOMContentLoaded', function() {
+  document.body.style.opacity = '1';
+});
+
+// Fade out antes de cambiar de página
+document.querySelectorAll('a').forEach(link => {
+  if (link.href && !link.hash) {
+    link.addEventListener('click', function(e) {
+
+      if (this.href.includes(window.location.hostname)) {
+        e.preventDefault();
+        document.body.style.opacity = '0';
+        setTimeout(() => {
+          window.location.href = this.href;
+        }, 200);
+      }
+    });
+  }
+});
