500de852a26ca0039545bc8aa42d52ed9b60bc58
emcahell <emmanuelltowers@gmail.com>
2025-06-30 21:11:09 -0400

    feat: agrege una transicion suave al cambiar de paginas (pags admin)

--- scripts/transitionPages.js
+++ scripts/transitionPages.js
@@ -4,17 +4,17 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 // Fade out antes de cambiar de página
-// document.querySelectorAll('a').forEach(link => {
-//   if (link.href && !link.hash) {
-//     link.addEventListener('click', function(e) {
+document.querySelectorAll('a').forEach(link => {
+  if (link.href && !link.hash) {
+    link.addEventListener('click', function(e) {
 
-//       if (this.href.includes(window.location.hostname)) {
-//         e.preventDefault();
-//         document.body.style.opacity = '0';
-//         setTimeout(() => {
-//           window.location.href = this.href;
-//         }, 60);
-//       }
-//     });
-//   }
-// });
+      if (this.href.includes(window.location.hostname)) {
+        e.preventDefault();
+        document.body.style.opacity = '0';
+        setTimeout(() => {
+          window.location.href = this.href;
+        }, 10);
+      }
+    });
+  }
+});
